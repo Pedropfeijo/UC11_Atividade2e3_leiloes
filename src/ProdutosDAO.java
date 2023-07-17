1c7f67de4a13f2464a986176c6e2ccd4e9a44611
Pedro Pasqualini <pedropasqualini@live.com>
2023-07-17 06:41:41 -0300

    Modificando classe ProdutosDAO

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -10,33 +10,95 @@
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProdutosDAO {
     
     Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    PreparedStatement st;
+    ResultSet rs;
+
+    public String url = "jdbc:mysql://localhost:3306/uc11_atividade1?serverTimezone=UTC";
+    public String user = "root";
+    public String password = "HackerMan1!";
+
+    public boolean conectar() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Conexão realizada com sucesso");
+            return true;
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Falha na conexão com o banco " + ex.getMessage());
+            return false;
+        }
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
+
+    public List<ProdutosDTO> listagem(String filtro) {
+        String sql = "select * from produtos";
+
+        if (!filtro.isEmpty()) {
+            sql = sql + " where nome like ?";
+        }
+
+        try {
+            st = conn.prepareStatement(sql);
+
+            if (!filtro.isEmpty()) {
+                st.setString(1, "%" + filtro + "%");
+            }
+
+            rs = st.executeQuery();
+            List<ProdutosDTO> lista = new ArrayList<>();
+
+            while (rs.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(rs.getInt("id"));
+                produto.setNome(rs.getString("nome"));
+                produto.setValor(rs.getInt("valor"));
+                produto.setStatus(rs.getString("status"));
+                lista.add(produto);
+            }
+
+            return lista;
+
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return null;
+        }
+
     }
-    
-    
-    
-        
+
+    public int salvar(ProdutosDTO produto) {
+        int status;
+        try {
+            st = conn.prepareStatement("INSERT INTO produtos VALUES(?,?,?,?)");
+            st.setInt(1, produto.getId());
+            st.setString(2, produto.getNome());
+            st.setInt(3, produto.getValor());
+            st.setString(4, produto.getStatus());
+            status = st.executeUpdate();
+            return status;
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return ex.getErrorCode();
+        }
+    }
+
+    public void desconectar() {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+
+        }
+    }
 }
 
