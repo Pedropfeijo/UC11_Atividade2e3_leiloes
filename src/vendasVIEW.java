aabdcd2fb954735a09abb577128c77e0c425a0f1
Pedro Pasqualini <pedropasqualini@live.com>
2023-07-17 07:12:42 -0300

    Formatação e detalhes de UI

--- src/vendasVIEW.java
+++ src/vendasVIEW.java
@@ -83,6 +83,11 @@ public class vendasVIEW extends javax.swing.JFrame {
         jLabel1.setText("LISTA DE PRODUTOS VENDIDOS");
 
         jButton1.setText("VOLTAR");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -115,6 +120,10 @@ public class vendasVIEW extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
